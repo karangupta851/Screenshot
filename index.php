59a48502aaa30288991ab975d53d7f921767d3fe
Karan Gupta <25882557+karangupta851@users.noreply.github.com>
2018-04-26 16:55:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 	<title>Take A Screenshot</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
